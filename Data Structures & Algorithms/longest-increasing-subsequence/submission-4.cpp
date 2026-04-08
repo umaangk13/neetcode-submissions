ec7a33af7d800b56c38432d39453dbd743a06ce7
Umaang Khambhati <144263757+umaangk13@users.noreply.github.com>
2026-04-08 21:28:34 +0530

    Add: longest-increasing-subsequence - submission-4

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> elements;
+        elements.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(elements.back() < nums[i]){
+                elements.push_back(nums[i]);
+            }
+            else{
+                auto it = lower_bound(elements.begin(),elements.end(),nums[i]);
+                int idx = it - elements.begin();
+                elements[idx] = nums[i];
+            }
+        }
+        return elements.size();
+    }
+};
