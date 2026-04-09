4b302a2639f3085341f6ad8beb1f34fc78f88e62
Umaang Khambhati <144263757+umaangk13@users.noreply.github.com>
2026-04-09 23:26:05 +0530

    Add: longest-increasing-path-in-matrix - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int n=0,m=0;
+    int Row[4] = {0,-1,0,1};
+    int Col[4] = {-1,0,1,0};
+    int f(int i, int j, vector<vector<int>>& matrix, vector<vector<int>> &dp){
+        if(dp[i][j] != -1) return dp[i][j];
+        int res=0;
+        for(int k=0;k<4;k++){
+            int r = i + Row[k];
+            int c = j + Col[k];
+            if(r<n && c<m && r>=0 && c>=0 && matrix[r][c] > matrix[i][j]){
+                res = max(res, f(r,c,matrix,dp));
+            }
+        }
+        return dp[i][j] = 1+res;
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        int result=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                result = max(result, f(i,j,matrix,dp));
+            }
+        }
+        return result;
+    }
+};
