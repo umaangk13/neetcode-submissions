77bb54c56aab665bb1c99980d1ca48429e187439
Umaang Khambhati <144263757+umaangk13@users.noreply.github.com>
2026-04-10 11:41:44 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        unordered_map<char,int> mp;
+        for(char c : s){
+            mp[c]++;
+        }
+
+        for(char c : t){
+            if(mp.find(c) == mp.end()) return false;
+            else{
+                mp[c]--;
+                if(mp[c] == 0) mp.erase(c);
+            }
+        }
+        return true;
+    }
+};
