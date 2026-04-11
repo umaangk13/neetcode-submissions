aeee9059032160359459f2631476a58c4b8da055
Umaang Khambhati <144263757+umaangk13@users.noreply.github.com>
2026-04-11 19:11:26 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int prod = 1, prod_z = 1, count = 0, count_z = 0;
+        for(int num : nums){
+            prod *= num;
+            if(num != 0){
+                prod_z *= num;
+                count_z++;
+            }
+            else count++;
+        }
+        if(count_z == 0 || count >= 2){
+            vector<int> res(nums.size(),0);
+            return res;
+        }
+        vector<int> result;
+        for(int num : nums){
+            if(num != 0) result.push_back(prod/num);
+            else result.push_back(prod_z);
+        }
+        return result;
+    }
+};
